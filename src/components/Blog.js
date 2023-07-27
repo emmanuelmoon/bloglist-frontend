4fb6a7b794db81312d0961f26f1b59f4c9f63de0
emmanuelmoon <emmenual123@gmail.com>
2023-07-27 09:42:16 +0500

    Completed 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,7 +1,36 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({blog}) => {
+  const [visible, setVisible] = useState(false)
+  const [buttonText, setButtonText] = useState('view')
+
+  const allVisible = { display: visible ? '': 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+    setButtonText(buttonText === 'view' ? 'hide': 'view')
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  console.log(blog)
+
+  return(
+  <div style={blogStyle}>
+    {blog.title} {blog.author} {' '}
+    <button onClick={toggleVisibility}>{buttonText}</button>
+    <div style={allVisible}>
+      {blog.url} <br />
+      likes {blog.likes} <button>like</button> <br />
+      {blog.user.username} <br />
+    </div>
+  </div>)
+}
 
 export default Blog
