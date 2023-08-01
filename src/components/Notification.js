7e5c80af6c4cdab8ac7fdf806a11b9d588309098
emmanuelmoon <emmenual123@gmail.com>
2023-08-01 09:41:10 +0500

    Completed 5.12

--- src/components/Notification.js
+++ src/components/Notification.js
@@ -1,9 +1,18 @@
-const Notification = ({message, messageClass}) => {
+import PropTypes from 'prop-types'
+
+const Notification = ({ message, messageClass }) => {
   if(message === '') {
     return null
   } else {
     return <div className={messageClass}>{message}</div>
   }
 }
+
+
+Notification.propsTypes = {
+  message: PropTypes.string.isRequired,
+  messageClass: PropTypes.object.isRequired
+}
+
 
 export default Notification
