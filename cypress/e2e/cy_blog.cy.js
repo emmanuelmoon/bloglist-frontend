c321e47eb792e857e2a84ada73e1cbcaaeaa996c
Emmanuel <emmenual123@gmail.com>
2023-08-15 09:57:55 +0500

    Completed 5.18

--- cypress/e2e/cy_blog.cy.js
+++ cypress/e2e/cy_blog.cy.js
@@ -4,9 +4,36 @@ describe('Blog app', function () {
     cy.visit('http://localhost:3000')
   })
 
-  it('Login form is show', function() {
+  it('Login form is showing', function() {
     cy.contains('Log in to application')
     cy.contains('username')
     cy.contains('password')
+  })
+
+  describe('Login', function() {
+    beforeEach(function(){
+      const user = {
+        username: 'moon',
+        name: 'Moon',
+        password: 'aspirine'
+      }
+      cy.request('POST', 'http://localhost:3003/api/users', user)
+    })
+    it('succeeds with correct credentials', function() {
+      cy.get('#username').type('moon')
+      cy.get('#password').type('aspirine')
+      cy.get('#login-button').click()
+
+      cy.contains('Moon logged in')
+    })
+
+    it('fails with wrong credentials', function() {
+      cy.get('#username').type('moon')
+      cy.get('#password').type('Aspirine')
+      cy.get('#login-button').click()
+
+      cy.get('.error').should('contain', 'wrong credentials')
+      cy.get('.error').should('have.css', 'color','rgb(255, 0, 0)')
+    })
   })
 })
